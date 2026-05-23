b6ea3a29076b83bd7e7f1390d6430b3b81be3eba
HiroyukiMakita <hiroyukimakita.pc@gmail.com>
2026-05-24 01:40:12 +0900

    fix:

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// @ts-ignore
 import "./51-modern-default.css";
+// @ts-ignore
 import "./style.css";
 import App from "./App.tsx";
 
